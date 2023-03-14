3254f1ab1be0140268ed97bfd8d81c862dbc624b
Jenifer <jnf.asm@gmail.com>
2023-03-14 18:07:49 -0300

    requisitos 3 diferente

--- script.js
+++ script.js
@@ -8,11 +8,43 @@ window.onload = () => {
         return `rgb(${red} ${green} ${blue})`;
     }
 
-    const paletaElement = document.getElementById('color-palette').children;
-    for (let index = 1; index < paletaElement.length; index += 1) {
-        paletaElement[index].style.backgroundColor = rgbRandom();
-    }
+    const blackColor = document.getElementsByClassName('color')[0];
+    blackColor.style.backgroundColor = 'black';
+
+    //botao cores aleatorias
+    const botaoCores = document.getElementById('button-random-color');
+    botaoCores.addEventListener('click', () => {
+        const colorPalet = document.getElementById('color-palette').children;
+        for (let index = 1; index < colorPalet.length; index += 1) {
+            colorPalet[index].style.backgroundColor = rgbRandom();
+        }
+    });
+
+
+
+
+
+
+
+
+
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// botaoCores.addEventListener('click', (event) => {
+//     paletaElement.style.backgroundColor = event.target.;
